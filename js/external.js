ebfc14e6c2324731dad83b7f369b101abc10dcb5
Austin Varnon <austinvarnon@gmail.com>
2020-07-24 10:31:28 -0500

    exercises completed

--- js/external.js
+++ js/external.js
@@ -1,10 +1,11 @@
 "use strict";
 
-var littleMermaid = prompt("How many Days did you Rent the Little Mermaid?");
-var brotherBear = prompt("How many days did you rent Brother Bear?");
-var hercules = prompt("How many days did you rent Hercules?");
+var littleMermaid = 3 //prompt("How many Days did you Rent the Little Mermaid?");
+var brotherBear = 5 //prompt("How many days did you rent Brother Bear?");
+var hercules = 1 //prompt("How many days did you rent Hercules?");
 
-var totalCost = 3 * (littleMermaid + brotherBear + hercules);
+var totalDays = littleMermaid + brotherBear + hercules;
+var totalCost = 3 * totalDays;
 console.log("Your total cost is: $" + totalCost.toFixed(2));
 
 
@@ -17,3 +18,14 @@ var payCheck = (400 * 6) + (380 * 4) + (350 * 10);
 console.log("Your weekly paycheck is: $" + payCheck.toFixed(2));
 
 
+var classIsFull = false;
+var scheduleHasConflicts = false;
+var canEnroll = ! classIsFull && ! scheduleHasConflicts;
+console.log(canEnroll);
+
+
+var itemsBought = 3;
+var offerValid = true;
+var isPremiumMember = true;
+var canApplyOffer = offerValid || itemsBought > 2;
+console.log(canApplyOffer);
