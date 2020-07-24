2d857b58b6d58f4b5f150b1e5d01f6d2808bf5ad
Austin Varnon <austinvarnon@gmail.com>
2020-07-24 10:00:23 -0500

    Last couple exercises

--- js/external.js
+++ js/external.js
@@ -1,12 +1,19 @@
 "use strict";
 
-console.log("Hello from External!");
-alert("Welcome to my Website!");
-var userInput = prompt("What is your favorite color?");
-console.log('The user entered: ' + userInput);
-var userInputLC = userInput.toLowerCase();
-    if (userInputLC == "blue") {
-        alert("Awesome, That is my favorite color too!");
-    } else {
-        alert("Thats cool, but blue is better.");
-    };
+var littleMermaid = prompt("How many Days did you Rent the Little Mermaid?");
+var brotherBear = prompt("How many days did you rent Brother Bear?");
+var hercules = prompt("How many days did you rent Hercules?");
+
+var totalCost = 3 * (littleMermaid + brotherBear + hercules);
+console.log("Your total cost is: $" + totalCost.toFixed(2));
+
+
+
+var google = 400;
+var amazon = 380;
+var fb = 350;
+
+var payCheck = (400 * 6) + (380 * 4) + (350 * 10);
+console.log("Your weekly paycheck is: $" + payCheck.toFixed(2));
+
+
