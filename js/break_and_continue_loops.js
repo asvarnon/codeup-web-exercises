c107af24f15489a22cc40621e2f5a0039a6f583d
Austin Varnon <austinvarnon@gmail.com>
2020-07-31 09:42:38 -0500

    simplified variation of code.

--- js/break_and_continue_loops.js
+++ js/break_and_continue_loops.js
@@ -1,13 +1,33 @@
 "use strict";
-(function (){
+//(function (){
 
+
+// function numberToSkip() {
+//     var userInput = prompt("Choose an odd number between 1 and 50 that you would like to skip?");
+//     for (var i = 1; i <= 50; i+=2) {
+//         if (userInput % 2 === 0) {
+//             alert("That is an even number.");
+//             return numberToSkip();
+//         }
+//         if (userInput < 0 || userInput > 50) {
+//             console.log("This number is not within the asked perameters.");
+//             break;
+//         }
+//         if (i === parseInt(userInput)) {
+//             console.log("Yikes! Skipping number " + userInput);
+//             continue;
+//         }
+//         console.log("Here is a random number: " + i);
+//     }
+//
+// }
 
 function numberToSkip() {
-    var userInput = prompt("Choose a number between 1 and 50 that you would like to skip?");
+    var userInput = prompt("Choose an odd number between 1 and 50 that you would like to skip?");
     for (var i = 1; i <= 50; i+=2) {
-        if (userInput < 0 || userInput > 50) {
-            console.log("This number is not within the asked perameters.");
-            break;
+        if (userInput % 2 === 0 || userInput < 0 || userInput > 50) {
+            alert("This number is not within the requested perameters.");
+            return numberToSkip();
         }
         if (i === parseInt(userInput)) {
             console.log("Yikes! Skipping number " + userInput);
@@ -15,7 +35,6 @@ function numberToSkip() {
         }
         console.log("Here is a random number: " + i);
     }
-
 }
 
 
@@ -25,6 +44,4 @@ function numberToSkip() {
 
 
 
-
-
-})();
+//})();
