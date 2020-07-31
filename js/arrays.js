a96932982747138862102143479085b78e88d2c1
Austin Varnon <austinvarnon@gmail.com>
2020-07-31 12:34:50 -0500

    iterating arrays half complete

--- js/arrays.js
+++ js/arrays.js
@@ -1,6 +1,28 @@
 "use strict";
-
 //(function(){
+
+function determineShapes() {
+    var shapes = ['square', 'rectangle', 'circle', 'triangle'];
+    // loop through the array and log the values
+    for (var i = 0; i < shapes.length; i++) {
+        console.log('The shape at index ' + i + ' is: ' + shapes[i]);
+    }
+}
+
+
+function codeUp() {
+    var sentence = "";
+    var words = ["Coding", "is", "fun", "at", "CodeUp!"];
+    words.forEach(function (word, i, words) {
+        sentence += word;
+        if (i < words.length - 1) {
+            sentence += " ";
+        } else {
+            sentence += "!";
+        }
+    });
+    console.log(sentence);
+}
 
 
 
