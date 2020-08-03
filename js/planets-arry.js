fb13a2d07d32e8d31d53e442fce0f987374b033b
Austin Varnon <austinvarnon@gmail.com>
2020-08-03 10:59:05 -0500

    Manipulating arrays finished

--- js/planets-arry.js
+++ js/planets-arry.js
@@ -1,4 +1,4 @@
-//(function (){
+(function (){
 "use strict";
 
 var planets = [
@@ -19,24 +19,31 @@ var planets = [
  */
 
 console.log('Adding "The Sun" to the beginning of the planets array.');
-console.log(planets);
+planets.unshift("The Sun");
+console.log("Q1" ,planets);
 
 console.log('Adding "Pluto" to the end of the planets array.');
-console.log(planets);
+planets.push("Pluto");
+console.log("Q2" ,planets);
 
 console.log('Removing "The Sun" from the beginning of the planets array.');
-console.log(planets);
+planets.shift();
+console.log("Q3" ,planets);
 
 console.log('Removing "Pluto" from the end of the planets array.');
-console.log(planets);
+planets.pop();
+console.log("Q4" ,planets);
 
 console.log('Finding and logging the index of "Earth" in the planets array.');
+console.log("Q5. Idex of Earth is: ", planets.indexOf("Earth"));
 
 console.log("Reversing the order of the planets array.");
-console.log(planets);
+planets.reverse();
+console.log("Q6" ,planets);
 
 console.log("Sorting the planets array.");
-console.log(planets);
+planets.sort();
+console.log("Q7" ,planets);
 
 
 
@@ -54,4 +61,4 @@ console.log(planets);
 
 
 
-//})();
+})();
