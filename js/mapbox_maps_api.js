810a8a969cc1f06605e5014a38f1f5555a3ba77f
Austin Varnon <austinvarnon@gmail.com>
2020-08-31 17:06:36 -0500

    feat: working on getting markers to show on map

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,28 +1,37 @@
 mapboxgl.accessToken = mapboxToken;
-geocode("2900 Wind River Ln # 130, Denton, TX 76210", mapboxToken)
-    .then(function (result) {
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-            center: [-97.1331, 33.2148], // starting position [lng, lat]
-            zoom: 11 // starting zoom
-        });
-        var popup = new mapboxgl.Popup()
-            .setHTML("Sidewalk Cafe")
-            .addTo(map);
 
-        var marker = new mapboxgl.Marker()
-            .setLngLat(result)
-            .addTo(map);
-
-        marker.setPopup(popup);
-        marker.togglePopup(popup);
-
-
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-97.1331, 33.2148], // starting position [lng, lat]
+    zoom: 11 // starting zoom
 });
 
-var restuarants = [
-    {name: "Sidewalk Cafe", address: "2900 Wind River Ln # 130, Denton, TX 76210"},
-    {name: "Raising Canes", address: "1511 S Loop 288, Denton, TX 76205"},
-    {name: "Pi Squared Pizza", address: "3200 S Interstate 35 #1150, Denton, TX 76210"}
+var restaurants = [
+    {name: "Sidewalk Cafe", address: "2900 Wind River Ln # 130, Denton, TX 76210", info: "Great Breakfast Food!"},
+    {name: "Raising Canes", address: "1511 S Loop 288, Denton, TX 76205", info: "Great Fried Chicken!"},
+    {name: "Pi Squared Pizza", address: "3200 S Interstate 35 #1150, Denton, TX 76210", info: "Great Pizza!"}
 ]
+
+restaurants.forEach(function (restaurant, mapboxToken, map) {
+    geocode(restaurant.address, mapboxToken)
+        .then(function (result) {
+
+            var popup = new mapboxgl.Popup()
+                .setHTML(restaurant.info)
+                // .addTo(map);
+
+            var marker = new mapboxgl.Marker()
+                .setLngLat(result)
+                .addTo(map)
+                .setPopup(popup);
+
+            popup.addTo(map);
+
+
+            // marker.setPopup(popup);
+            // marker.togglePopup(popup);
+
+        });
+});
+
