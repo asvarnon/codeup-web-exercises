307e5b3117cdbf125e5ca7422b3e1abcef876094
Austin Varnon <austinvarnon@gmail.com>
2020-09-04 18:28:47 -0500

    ending th day

--- js/es6Experiment.js
+++ js/es6Experiment.js
@@ -1,9 +1,17 @@
 "use strict";
 
+// es6 syntax (including template strings)
+function sayHello(name = 'Austin') {
+    return `Hello, ${name}!`;
+}
+
+sayHello(); // "Hello, World!"
+sayHello('codeup'); // "Hello, codeup!"
 
 
-
-
+const sayHelloArrow = (name = "Austin") => {
+    return `Hello, ${name}. I am Joe`;
+}
 
 
 
