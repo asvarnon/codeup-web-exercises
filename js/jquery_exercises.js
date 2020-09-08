21e77b42609b6fd90a09b3843d8442f876878f13
Austin Varnon <austinvarnon@gmail.com>
2020-09-08 11:48:29 -0500

    done with selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,20 @@
 "use strict";
 
-$(document).ready(() => {
-    alert('the DOM has finished loading!');
-    });
+// $(document).ready(() => {
+//     alert('the DOM has finished loading!');
+//     });
+
+$('#container').css('border', '1px solid black');
+
+$('.important').css('background-color', 'yellow');
+
+$('.codeup').css('border', '1px solid red');
+
+$('li, .codeup').css("font-size", "20px");
+
+
+
+
+
+
+
