418278534714c85b369c5715fe7a48ed905f77b9
Austin Varnon <austinvarnon@gmail.com>
2020-09-08 15:59:08 -0500

    working on jquery events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -12,9 +12,20 @@ $('.codeup').css('border', '1px solid red');
 
 $('li, .codeup').css("font-size", "20px");
 
+// $('#title-main').click(() => {
+//     alert('This is the main header.');
+// });
 
+// $('#title-main').dblclick(() => {
+//     alert('This is the main header that was double clicked.');
+// });
 
+$('#title-main').hover(
+    () => {
+        $(this).css('background-color', '#FF0');
+    },
+    () => {
+        $(this).css('background-color', '#FFF');
+    });
 
-
-
-
+//note from Kenneth, Tailwind is a css framework that is similar to bootstrap
