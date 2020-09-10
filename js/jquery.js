21f73a88ae1eaac17f35a65ebce6e9d1420e5345
Austin Varnon <austinvarnon@gmail.com>
2020-09-10 15:01:18 -0500

    FEAT: Array recorded, working on completion alert.

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,49 @@
 "use strict";
 
-const inputListString = ['abcdefghijklmnopqrstuvwxyz0123456789'];
-const inputListArr = inputListString.split(', ');
-console.log(inputListArr);
+var inputList = [];
+const cheatCodeKeyID = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+
+$(document).ready(function (){
+
+    // var inputList = [];
+    // const cheatCodeKeyID = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    //correct sequence
+
+
+    // $('#sequence-reset').click(function (){
+    //     return inputList = [];
+    // });
+
+    $(document).keyup(function(event){
+        inputList.push(event.keyCode);
+        console.log(event.keyCode);
+        for (var i = 0; i < inputList.length; i++) {
+            if (inputList[i] !== cheatCodeKeyID[i]){
+                inputList = [];
+            }
+        }
+    });
+
+    $(document).keyup(function(event){
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
 
 
 
