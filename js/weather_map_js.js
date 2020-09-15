5a160db0f2f083c1e455390b462b8c0688f76a3b
Austin Varnon <austinvarnon@gmail.com>
2020-09-15 12:28:56 -0500

    got data to show, working on formatiing it.

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -22,15 +22,14 @@
             let forecast = "";
             // for(let i = 0; i < data.length; i++){
                 forecast +=
-                    "<div class=\"col mb-4\">\n" +
-                    "    <div class=\"card\">\n" +
-                    "      <img src=\"../img/planning.jpg\" class=\"card-img-top\" alt=\"...\">\n" +
-                    "      <div class=\"card-body\">\n" +
-                    "        <h5 class=\"card-title\">"+ data.city.name +"</h5>\n" +
-                    "        <p class=\"card-text\">"+ data.list.main.temp +"</p>\n" +
-                    "      </div>\n" +
-                    "    </div>\n" +
-                    "  </div>";
+                    "<div class=\"card bg-dark text-white\">\n" +
+                    "  <img src=\"../img/darkBackground.jpg\" class=\"card-img\" alt=\"...\">\n" +
+                    "  <div class=\"card-img-overlay\">\n" +
+                    "    <h5 class=\"card-title\"> Location: " +data.name+ "</h5>\n" +
+                    "    <p class=\"card-text\"> Temperature: "+ data.list[0].temp +"</p>\n" +
+                    "    <p class=\"card-text\"> Date & Time: "+ data.list[0].dt_txt +"</p>\n" +
+                    "  </div>\n" +
+                    "</div>";
             // }
             $('#forecast').append(forecast);
         });
