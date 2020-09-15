b97e37e7592f76b5084722eb74fddb954245dc91
Austin Varnon <austinvarnon@gmail.com>
2020-09-15 14:11:42 -0500

    inidividual cards displayed in row across screen

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -22,15 +22,56 @@
             let forecast = "";
             // for(let i = 0; i < data.length; i++){
                 forecast +=
-                    "<div class=\"card bg-dark text-white\">\n" +
-                    "  <img src=\"../img/darkBackground.jpg\" class=\"card-img\" alt=\"...\">\n" +
-                    "  <div class=\"card-img-overlay\">\n" +
-                    "    <h5 class=\"card-title\"> Location: " +data.name+ "</h5>\n" +
-                    "    <p class=\"card-text\"> Temperature: "+ data.list[0].temp +"</p>\n" +
-                    "    <p class=\"card-text\"> Date & Time: "+ data.list[0].dt_txt +"</p>\n" +
-                    "  </div>\n" +
+                    "<div>\n" +
+                    "<div class=\"card-header\">\n" + data.city.name +
+                    "        </div>\n" +
+                    "        <ul class=\"list-group list-group-flush\">\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.temp +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].dt_txt +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.feels_like +"</li>\n" +
+                    "        </ul>" +
+                    "</div>"
+                    +
+                    "<div>\n" +
+                    "<div class=\"card-header\">\n" + data.city.name +
+                    "        </div>\n" +
+                    "        <ul class=\"list-group list-group-flush\">\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.temp +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].dt_txt +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.feels_like +"</li>\n" +
+                    "        </ul>" +
+                    "</div>"
+                    +
+                    "<div>\n" +
+                    "<div class=\"card-header\">\n" + data.city.name +
+                    "        </div>\n" +
+                    "        <ul class=\"list-group list-group-flush\">\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.temp +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].dt_txt +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.feels_like +"</li>\n" +
+                    "        </ul>" +
+                    "</div>"
+                    +
+                    "<div>\n" +
+                    "<div class=\"card-header\">\n" + data.city.name +
+                    "        </div>\n" +
+                    "        <ul class=\"list-group list-group-flush\">\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.temp +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].dt_txt +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.feels_like +"</li>\n" +
+                    "        </ul>" +
+                    "</div>"
+                    +
+                    "<div>\n" +
+                    "<div class=\"card-header\">\n" + data.city.name +
+                    "        </div>\n" +
+                    "        <ul class=\"list-group list-group-flush\">\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.temp +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].dt_txt +"</li>\n" +
+                    "            <li class=\"list-group-item\">"+ data.list[0].main.feels_like +"</li>\n" +
+                    "        </ul>" +
                     "</div>";
-            // }
+
             $('#forecast').append(forecast);
         });
 
