1439e31b714f7dfa96c1e20730401796db8de8f4
Austin Varnon <austinvarnon@gmail.com>
2020-09-15 12:05:19 -0500

    wokring on weather map.

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -1,7 +1,42 @@
 (function(){
     "use strict";
 
+    $(document).ready(function(){
 
+        var openWeatherAPIURL = "http://api.openweathermap.org/data/2.5/weather";
+        $.get(openWeatherAPIURL,{
+            "APPID": OWM_Key,
+            "q": "Dallas, TX, US",
+            "units": "imperial"
+        }).done(function(data){
+            // console.log(data);
+        })
+
+        var openForecastAPIURL = "http://api.openweathermap.org/data/2.5/forecast";
+        $.get(openForecastAPIURL, {
+            "APPID": OWM_Key,
+            "q": "Dallas, TX, US",
+            "units": "imperial"
+        }).done(function(data){
+            console.log(data);
+            let forecast = "";
+            // for(let i = 0; i < data.length; i++){
+                forecast +=
+                    "<div class=\"col mb-4\">\n" +
+                    "    <div class=\"card\">\n" +
+                    "      <img src=\"../img/planning.jpg\" class=\"card-img-top\" alt=\"...\">\n" +
+                    "      <div class=\"card-body\">\n" +
+                    "        <h5 class=\"card-title\">"+ data.city.name +"</h5>\n" +
+                    "        <p class=\"card-text\">"+ data.list.main.temp +"</p>\n" +
+                    "      </div>\n" +
+                    "    </div>\n" +
+                    "  </div>";
+            // }
+            $('#forecast').append(forecast);
+        });
+
+
+    });
 
 
 
