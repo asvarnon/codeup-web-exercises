7f629cf3937576f754dc80f478f6332f042758e3
Austin Varnon <austinvarnon@gmail.com>
2020-09-21 12:30:18 -0500

    working on fetching correct data

--- js/promises.js
+++ js/promises.js
@@ -1,8 +1,56 @@
 "use strict";
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~lecture examples~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-fetch("")
-    .then((response) => console.log(response))
-    .catch((error) => console.error(error));
+// const url = "https://api.github.com/users";
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => console.error(error));
+
+//logs each user with multiple functions within the same .then
+// fetch(url)
+//     .then(response => {
+//         response.json().then(users => {
+//             users.forEach( user => {
+//                 console.log(user);
+//             });
+//         });
+//     })
+//     .catch(error => console.error(error));
+
+//3rd way
+// const getGithubUsers = () => fetch(url).get(response => response.json());
+
+// getGithubUsers()
+//     .then(users => {
+//             users.forEach( user => {
+//                 console.log(user);
+//             });
+//         }).catch(error => console.log(error));
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Exercise~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const url = "https://api.github.com/users";
+const wycatsURL = "https://api.github.com/users/wycats/repos"
+// fetch(url, {headers: {'Authorization': githubToken}})
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+const userCommits = (name) => {
+    let userURL = `https://api.github.com/users/${name}/repos`
+    fetch(userURL, {headers: {'Authorization': githubToken}})
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+}
+userCommits("wycats");
+
+
+
+
 
 
